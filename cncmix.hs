2a0fc241987d2c13514f1ba247f15a748fcbb4de
Jim Saxon <Sonarpulse@Linuxmail.org>
2012-11-02 03:16:04 -0400

    For now I am just going to implement TD mixes, and hack it together in one file. Later I can work and splitting things up and keeping a nice interface like Codec.Archive.Tar

--- cncmix.hs
+++ cncmix.hs
@@ -1,11 +1,85 @@
 module Codec.Archive.CnCMix where
 
-import Codec.Archive.CnCMix.Hashfunc
-
-import qualified Data.ByteString.Lazy as BL
-import Data.Binary.Get
 import Data.Word
+import Data.Int
 import Data.Bits
 import Data.Char
 
---interface like Codec.Archive.Tar to go here
+
+--
+-- Datatypes
+--
+
+
+import Data.ByteString.Lazy (ByteString)
+import Data.Int (Int64)
+
+newtype MixArchive = MixArchive { mix :: MixArchive2 }
+  deriving Show
+
+-- | A Command & Conquer MIX archive.
+data MixArchive2 = MixArchive2
+    {
+      -- | most importantly, gives filecount
+      masterHeader :: MixHeader,
+      -- | length and offset for each file
+      entryHeaders :: [MixEntry],
+      -- | the files themselves
+      entryData :: [ByteString]
+    }
+  deriving Show
+
+
+-- | The Master header for a Mix
+data MixHeader = MixHeader
+    {
+      -- | number of internal files
+      numFiles :: Int16,  
+      -- | size of the body, not including this header and the index
+      totalSize :: Int32
+    }
+  deriving Show
+
+-- | A MIX archive entry for a file
+data MixEntry = MixEntry
+    {
+      -- | id, used to identify the file instead of a normal name     
+      id :: Word32,
+      -- | offset from start of body
+      offset :: Int32,
+      -- | size of this internal file
+      size :: Int32
+    }
+  deriving Show
+
+
+--
+-- Hashing Function
+--
+
+
+filenameTOid :: [Word32] -> Word32
+filenameTOid = foldl rotsum 0
+
+rotsum :: Word32 -> Word32 -> Word32
+rotsum a b = a + (b `rotateL` 1)
+
+
+stringTOfilename :: [Char] -> [Word32]
+stringTOfilename [] = []
+stringTOfilename a
+  | length a<=4 = (s2f1 1 a) : []
+  | length a>4  = (s2f1 1 a) : (stringTOfilename ((tail . tail . tail . tail) a))
+
+
+s2f1 :: Int -> [Char] -> Word32
+s2f1 5 xs  = 0
+s2f1 xk []  = 0
+s2f1 k xs 
+  | k <= 4 = shiftL (charTOasciiword32 $ head xs) (8*k) + s2f1 (k+1) (tail xs)
+
+
+charTOasciiword32 :: Char -> Word32
+charTOasciiword32 c 
+  | isAscii c = fromIntegral $ fromEnum $ toUpper c
+  | otherwise = error "non-ascii"
