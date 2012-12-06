feaf7efd6a9bc2921de29e3787fe24a8e767576a
Jim Saxon <Sonarpulse@Linuxmail.org>
2012-12-05 21:23:34 -0500

    cncmix's 'create' and 'remove' combined into 'mod'

--- Main.hs
+++ Main.hs
@@ -15,92 +15,82 @@ import Codec.Archive.CnCMix
 import Codec.Archive.CnCMix.Backend
 
 
-data Basic = Info    { mixPath1   :: FilePath
-                     , lType      :: Bool
-                     , lCont      :: Bool
+data Basic = Info    { mixPath1  :: FilePath
+                     , lType     :: Bool
+                     , lCont     :: Bool
                      }
-           | Create  { mixPath2   :: FilePath
-                     , inputFiles :: [FilePath]
-                     , mixType    :: CnCGame
-                     , safe       :: Bool
+           | Mod     { mixPath2  :: FilePath
+                     , addFs     :: [FilePath]
+                     , rmFs      :: [String]
+                     , mixType   :: CnCGame
+                     , safe      :: Bool
                      }
-           | Extract { mixPath3   :: FilePath
-                     , outputDir  :: FilePath
-                     }
-           | Remove  { mixPath4   :: FilePath
-                     , namesIds   :: [String]
+           | Extract { mixPath3  :: FilePath
+                     , outputDir :: FilePath
                      }
            deriving (Typeable, Data, Eq)
 
 
 instance Attributes Basic where
   attributes _ = group "Options" [
-    mixPath1   %> [ Short ['I']
-                  , Long ["mix"]
-                  , Help "the path to the Mix print information about"
-                  , ArgHelp "Path"
-                  , Required True
-                  ],
-    lType      %> [ Short ['t']
-                  , Long  ["print-type"]
-                  , Help "Should CnCMix print the type of Mix?"
-                  , Invertible True
-                  , Default True
-                  ],
-    lCont      %> [ Short ['c']
-                  , Long  ["list-files"]
-                  , Help "Should CnCMix print a list of the Mix's content?"
-                  , Invertible True
-                  , Default True
-                  ],
-    ---------------
-    mixPath2   %> [ Short ['O']
-                  , Long ["mix"]
-                  , Help "the path to the Mix to be created"
-                  , ArgHelp "Path"
-                  , Required True
-                  ],
-    inputFiles %> [ Help "the files from which to create the Mix. Folders will be recurred into"
-                  , ArgHelp "Paths"
-                  , Extra True
-                  , Required True
-                  ],
-    mixType    %> [ Short ['t']
-                  , Long ["type"]
-                  , Help "which type of Mix should be created?"
-                  , ArgHelp "Game-Name"
-                  --, Required True
-                  , Default TiberianDawn
-                  ],
-    safe       %> [ Short ['s']
-                  , Help "should CnCMix check for ID collisions?"
-                  , Invertible True
-                  ],
-    ---------------
-    mixPath3   %> [ Short ['I']
-                  , Long ["mix"]
-                  , Help "the path to the Mix to be extracted"
-                  , ArgHelp "Path"
-                  , Required True
-                  ],
-    outputDir  %> [ Short ['O']
-                  , Help "the directory into which to extract the files"
-                  , ArgHelp "Path"
-                  , Required True
-                  ],
-    ---------------
-    mixPath4   %> [ Short ['I']
-                  , Long ["mix"]
-                  , Help "the path to the Mix to be filtered"
-                  , ArgHelp "Path"
-                  , Required True
-                  ],
-    namesIds   %> [ Help $ "the name or ID of each file to be removed."
-                    ++ " IDs should be prefixed with \'0x\' and written in hexadecimal"
-                  , Extra True
-                  , ArgHelp "Names and IDs"
-                  , Required True
-                  ]
+    mixPath1  %> [ Short ['I']
+                 , Long ["mix"]
+                 , Help "the path to the Mix print information about"
+                 , ArgHelp "Path"
+                 , Required True
+                 ],
+    lType     %> [ Short ['t']
+                 , Long  ["print-type"]
+                 , Help "Should CnCMix print the type of Mix?"
+                 , Invertible True
+                 , Default True
+                 ],
+    lCont     %> [ Short ['c']
+                 , Long  ["list-files"]
+                 , Help "Should CnCMix print a list of the Mix's content?"
+                 , Invertible True
+                 , Default True
+                 ],
+    -------------
+    mixPath2 %> [ Short ['O']
+                , Long ["mix"]
+                , Help "the path to the Mix to be created or modified"
+                , ArgHelp "Path"
+                , Required True
+                ],
+    addFs     %> [ Short ['a']
+                 , Long ["add"]
+                 ,  Help "the files from which to create the Mix. Folders will be recurred into"
+                 , ArgHelp "Paths"
+                 ],
+    rmFs      %> [ Short ['d']
+                 , Long ["remove"]
+                 , Help $ "the name or ID of each file to be removed."
+                   ++ " IDs should be prefixed with \'0x\' and written in hexadecimal"
+                 , ArgHelp "Names and IDs"
+                 ],
+    mixType   %> [ Short ['t']
+                 , Long ["type"]
+                 , Help "which type of Mix should be created?"
+                 , ArgHelp "Game-Name"
+                 , Default TiberianDawn
+                 ],
+    safe      %> [ Short ['s']
+                 , Help "should CnCMix check for ID collisions?"
+                 , Invertible True
+                 ],
+    --------------
+    mixPath3  %> [ Short ['I']
+                 , Long ["mix"]
+                 , Help "the path to the Mix to be extracted"
+                 , ArgHelp "Path"
+                 , Required True
+                 ],
+    outputDir %> [ Short ['O']
+                 , Help "the directory into which to extract the files"
+                 , ArgHelp "Path"
+                 , Required True
+                 ]
     ]
 
 
@@ -121,65 +111,53 @@ getDirContentsRecursive p =
 
 
 instance RecordCommand Basic where
-  run' cmd@(Info    {}) _ =
+  run' cmd@(Info {}) _ =
     do when sType $ putStrLn . ("Mix Type: " ++) =<< liftM (show . detect) (L.readFile mPath)
        when sCont $ do putStrLn . ("File Count: " ++) . show . length =<< mix
                        mapM_ (putStrLn . \(a,b) -> a ++ " " ++ b) . showFileHeaders =<< mix
-    where sType = lType cmd
-          sCont = lCont cmd
+    where sType = lType    cmd
+          sCont = lCont    cmd
           mPath = mixPath1 cmd
           mix   = liftM dispatchDecode $ L.readFile mPath
 
-  run' cmd@(Create  {}) _ =
+  run' cmd@(Mod {}) _ =
     do pred <- doesFileExist mPath
-       new  <- dispatchReadFile3s mType =<< (liftM concat . mapM getDirContentsRecursive $ inputFiles cmd)
+       aFs  <- dispatchReadFile3s mType =<< (liftM concat . mapM getDirContentsRecursive $ addFs cmd)
        tmpF <- uncurry openBinaryTempFile $ splitFileName mPath
        nMix <- if pred
                then do old <- dispatchDecode `liftM` L.readFile mPath
-                       return $ if isS
-                                then mergeSafeRecursiveFile3s
-                                     (mergeSafeRecursiveFile3s [] old)
-                                     new
-                                else mergeFile3s old new
-               else return    $ if isS
-                                then mergeSafeRecursiveFile3s [] new
-                                else new
+                       mid <- return $ if isS
+                                       then mergeSafeRecursiveFile3s
+                                            (mergeSafeRecursiveFile3s [] old)
+                                            aFs
+                                       else mergeFile3s old aFs
+                       return $ removeFile3s mid
+                         $ map (dispatchUpdateFile3 mType . \a -> File3 a 0 L.empty) rFs
+               else return  $ if isS
+                              then mergeSafeRecursiveFile3s [] aFs
+                              else aFs
        L.hPut (snd tmpF) $ dispatchEncode mType nMix
        hClose $ snd tmpF
        renameFile (fst tmpF) mPath
     where mType = mixType  cmd
           mPath = mixPath2 cmd
-          isS   = safe cmd
+          rFs   = rmFs     cmd
+          isS   = safe     cmd
 
   run' cmd@(Extract {}) _ = writeFile3s oDir
                             =<< liftM dispatchDecode (L.readFile mPath)
     where oDir  = outputDir cmd
-          mPath = mixPath3 cmd
-
-  run' cmd@(Remove  {}) _ =
-    do mType <- liftM detect $ L.readFile mPath
-       tmpF <- uncurry openBinaryTempFile $ splitFileName mPath
-
-       (L.hPut (snd tmpF) . dispatchEncode mType
-        . flip removeFile3s (map (dispatchUpdateFile3 mType . \a -> File3 a 0 L.empty) rkeys))
-         =<< liftM dispatchDecode (L.readFile mPath)
-
-       hClose $ snd tmpF
-       renameFile (fst tmpF) mPath
-    where mPath  = mixPath4 cmd
-          rkeys  = namesIds cmd
+          mPath = mixPath3  cmd
 
 
   mode_summary Info    {} = "print information about a Mix"
-  mode_summary Create  {} = "create a new Mix archive"
+  mode_summary Mod     {} = "create or modify a Mix archive"
   mode_summary Extract {} = "extract files from a Mix"
-  mode_summary Remove  {} = "remove files from a Mix"
 
 
 main :: IO ()
 main = do x <- getArgs
           dispatchR [] x >>= \y -> case y of
             cmd@(Info    {}) -> run' cmd x
-            cmd@(Create  {}) -> run' cmd x
+            cmd@(Mod     {}) -> run' cmd x
             cmd@(Extract {}) -> run' cmd x
-            cmd@(Remove  {}) -> run' cmd x
